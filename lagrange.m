509d7df7c322059e80795b1a88d8cf9e2c2a09df
Kastuks <71520231+Kastuks@users.noreply.github.com>
2021-11-25 15:27:42 +0200

    Update lagrange.m

--- lagrange.m
+++ lagrange.m
@@ -11,10 +11,15 @@ f(x) = atan(x)*sin(x);
 dxn = diff(f(x), n1);
 xn = @(x) x^(n1);
 
-%ser = series(dxn, x, n1) + rn;
-%ser
-func = subs(dxn, x, c) / factorial(n1) * x^6;
 
+func = subs(dxn, x, c) / factorial(n1) * x^6;
+func
+%func =
+%-x^6*((atan(c)*sin(c))/720 - cos(c)/(18*(c^2 + 1)^2) - cos(c)/(5*(c^2 + 1)^3) - 
+%cos(c)/(120*(c^2 + 1)) + (c*sin(c))/(24*(c^2 + 1)^2) + (c*sin(c))/(2*(c^2 + 1)^3) + 
+%(c*sin(c))/(c^2 + 1)^4 + (2*c^2*cos(c))/(9*(c^2 + 1)^3) + (12*c^2*cos(c))/(5*(c^2 + 1)^4) - 
+%(16*c^4*cos(c))/(5*(c^2 + 1)^5) - (c^3*sin(c))/(c^2 + 1)^4 - (16*c^3*sin(c))/(3*(c^2 + 1)^5) + 
+%(16*c^5*sin(c))/(3*(c^2 + 1)^6))
 neg_function = -(abs(dxn));
 neg_lab_function = matlabFunction(neg_function);
 
@@ -40,10 +45,10 @@ upper_bounds = [];
 
 for i= 1:3
   upper_bound = Ms(i) / factorial(n1) * Ns(i);
- % upper_bound
   upper_bounds(i) = (upper_bound);
 end
 format long G
 upper_bounds
-
+% upper_bounds =
+%       0.00411999405243267      2.63003356583299e-07      2.12372576358679e-19
 
